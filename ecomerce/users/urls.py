928282d41b862b0085dc6400db9284e3b41aa8f8
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-02-20 16:35:29 +0300

    Delete urls.py
    merged the main urls with the user urls

--- ecomerce/users/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns=[
-    path('register', views.register, name='user_register'),
-    path('',views.login_request,name='login'),
-    path('logout/',views.logout_request,name='logout')
-]
