7aecdccc6710f96c9fcf386978fdf5554e3a4073
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-02-27 18:57:52 +0300

    Update views.py
    Updated the views

--- ecomerce/store/views.py
+++ ecomerce/store/views.py
@@ -15,7 +15,7 @@ def store(request):
         cartItems = order['get_cart_items']
 
     products = Product.objects.all()
-    context= {'products':products}
+    context= {'products':products,'cartItems': cartItems}
     return render(request,'store/store.html',context)
    
 
@@ -24,12 +24,12 @@ def cart(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer,complete=False)
         items = order.orderitem_set.all()
-        
+        cartItems = order['get_cart_items']
     else:
         items = []
         order={'get_cart_total':0,'get_cart_items':0}
 
-    context= {'items':items, 'order':order}
+    context= {'items':items, 'order':order,'cartItems':cartItems}
     return render(request,'store/cart.html',context)
 
 def checkout(request):
@@ -38,11 +38,12 @@ def checkout(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer,complete=False)
         items = order.orderitem_set.all()
+        cartItems = order['get_cart_items']
     else:
         items = []
         order={'get_cart_total':0,'get_cart_items':0}
 
-    context= {'items':items, 'order':order}
+    context= {'items':items, 'order':order,'cartItems':cartItems}
     return render(request,'store/checkout.html',context)
 
 def updateItem(request):
