fa312287472171d795a455bc1d773159a78eef10
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-02-28 17:52:16 +0300

    Update views.py
    created a process order view

--- ecomerce/store/views.py
+++ ecomerce/store/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import *
 from django.http import JsonResponse
 import json
+import datetime
 # Create your views here.
 def store(request):
     if request.user.is_authenticated:
@@ -26,6 +27,8 @@ def cart(request):
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
+        cart = json.loads(request.COOKIES['cart'])
+        print('Cart',cart)
         items = []
         order={'get_cart_total':0,'get_cart_items':0,'shipping':False}
         cartItems = order['get_cart_items']
@@ -75,4 +78,28 @@ def updateItem(request):
     return JsonResponse('Item was added', safe=False)
 
 def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer,complete=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+
+        if total == order.get_cart_total:
+            order.complete = True
+        order.save()
+
+        if order.shipping ==True:
+            ShippingAddress.objects.create(
+                customer = customer,
+                order = order,
+                address = data['shipping']['address'],
+                city = data['shipping']['city'],
+                state = data['shipping']['state'],
+                zipcode = data['shipping']['zipcode'],
+            )
+    else:
+        print("User is not logged in...")
     return JsonResponse("Payment complete",safe=False)
