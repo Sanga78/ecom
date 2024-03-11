9f71318f322cdc26bb285be19e23b78d1806aef3
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-03-11 18:19:04 +0300

    Update urls.py
    updated reset password urls

--- ecomerce/ecomerce/urls.py
+++ ecomerce/ecomerce/urls.py
@@ -20,13 +20,18 @@ from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 from users import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('store.urls')),
     path('register', user_views.register, name='register'),
-    path('login',user_views.login_request,name='login'),
+    path('login/',user_views.login_request,name='login'),
     path('logout/',user_views.logout_request,name='logout'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
