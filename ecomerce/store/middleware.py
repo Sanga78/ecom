2e8d65204e8c1c2d832b2fb7c04ceacc11136c17
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-03-13 21:41:52 +0300

    Update middleware.py
    added a middleware for detecting bruteforce attacks and limiting the number of login attempts

--- ecomerce/store/middleware.py
+++ ecomerce/store/middleware.py
@@ -1,6 +1,7 @@
 from typing import Any
 from django.http import HttpResponseForbidden
 from django.core.cache import cache
+from ecomerce.ecomerce import settings
 
 class DDoSMiddleware:
     def __init__(self, get_response):
@@ -27,3 +28,27 @@ class DDoSMiddleware:
     def __call__(self, request) :
         return self.process_request(request) or self.get_response(request)
     
+class BruteForceProtection:
+    def __init__(self, get_response):
+
+        self.get_response = get_response
+    
+    def __call__(self, request) :
+        response = self.get_response(request)
+        print(response)
+        #check if the request is for the login page
+        if request.path == settings.LOGIN_URL and request.method == "POST":
+            #get the IP address of the client
+            ip_address = request.META.get("REMOTE_ADDR")
+
+            #increment the failed login attempt count for this IP address
+            cache_key = f"login_attempts:{ip_address}"
+            login_attempts = cache.get(cache_key, 0)
+            if response.status_code != 200:
+                cache.set(cache_key, login_attempts + 1, timeout=settings.BRUTE_FORCE_TIMEOUT)
+
+                #If the login attempts exceed the threshold, block further attempts
+            if login_attempts >= settings.BRUTE_FORCE_THRESHOLD:
+                return HttpResponseForbidden(f"Too many login attempts. Please try again later after {settings.BRUTE_FORCE_TIMEOUT} seconds.")
+            
+        return response
