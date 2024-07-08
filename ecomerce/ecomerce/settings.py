f84826cc0c3b6b50eb37b2acfca30bb674a7baaa
kelvin sanga <97375935+Sanga78@users.noreply.github.com>
2024-07-08 21:40:11 +0300

    Update settings.py

--- ecomerce/ecomerce/settings.py
+++ ecomerce/ecomerce/settings.py
@@ -153,8 +153,8 @@ MPESA_INITIATOR_SECRET_CREDENTIAL = ""
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS=True
 EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER='sangakelvin026@gmail.com'
-EMAIL_HOST_PASSWORD='hkvq raci sfps ruau'
+EMAIL_HOST_USER=''
+EMAIL_HOST_PASSWORD=''
 EMAIL_PORT=587
 
 LOGIN_URL = reverse_lazy('login')
